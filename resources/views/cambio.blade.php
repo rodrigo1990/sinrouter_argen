cc327091dfdec9a34e40e6e80d128ef4265536a2
rodrigo <mcd77.1990@gmail.com>
2019-09-03 18:27:30 -0300

    mapa en cambio

--- resources/views/cambio.blade.php
+++ resources/views/cambio.blade.php
@@ -10,7 +10,10 @@
 			 <li class="first" style="">
 				
 				<ul>
-				<li><h1 class="text-center white"><b>CAMBIO HOY</b></h1></li>
+				<li>
+					<h1 class="text-center white"><b>CAMBIO HOY</b></h1>
+					 <h3 class="text-center white"> COMPRA Y VENTA DE MONEDA EXTRANJERA</h3>
+				</li>
 				<li>
 					<h2 clasS="text-center">1 USD = ${{$archivo[5]}}</h2>
 					<div>Compra: <span id="dolar-compra"></span></div>
@@ -172,8 +175,33 @@
 		<b>¿DÓNDE CAMBIO?</b>
 	</h2>
 	<div class="row margin-top-25">
-		<div id="map"></div>
+		<section id="sucursales">
+		<div class="container">
+		<div class="col-lg-4 col-md-4 col-sm-12">
+					<ul>
+						<li class="blue margin-top-10"><h3>ZONA NOROESTE</h3></li>
+						<li class="margin-top-10 margin-left-20">- <a onClick="zoomOnLocation(-34.435619,-58.563499);">San Fernando</a></li>
+						<li class="margin-top-10 margin-left-20">- <a onClick="zoomOnLocation(-34.545593,-58.707374);">San Miguel</a></li>
+					
+					<!--  	<li class="margin-top-10 margin-left-20">- <a onClick="zoomOnLocation(-34.681403,-58.556553);">San Justo</a></li>-->
+						<li class="margin-top-10 blue"><h3>ZONA SUR</h3></li>
+						<li class="margin-top-10 margin-left-20">- <a onClick="zoomOnLocation(-34.722831,-58.259495);">Quilmes</a></li>
+						<!-- <li class="margin-top-10 margin-left-20">- <a onClick="zoomOnLocation(-34.660564,-58.367637);">Avellaneda</a></li>
+						<li class="margin-top-10 margin-left-20">- <a onClick="zoomOnLocation(-34.760503,-58.206957);">Berazategui</a></li>
+						<li class="margin-top-10 margin-left-20">- <a onClick="zoomOnLocation(-34.708119,-58.389905);">Lanús</a></li>
+						<li class="margin-top-10 margin-left-20">- <a onClick="zoomOnLocation(-34.762114,-58.398490);">Lomas De Zamora</a></li>
+					
+						<li class="margin-top-10 margin-left-20">- <a onClick="zoomOnLocation(-34.804170,-58.274902);">Varela</a></li> -->
+						
+					</ul>
+		</div>
+		<div class="col-lg-8 col-md-8 col-sm-12 map">
+			<div id="map"></div>			
+
+		</div>
+		</div>
 	</div>
+	</section>
 </section>
 
 
@@ -299,12 +327,7 @@
    });
 
 
- 	window.centerGlobal = {lat: -34.562872, lng: -58.451280};
-  	window.markers=[];
- 	window.locations=[];
- 	window.map;
-
- 	locations.push(['<b>SAN MIGUEL </b><br><br>Presidente Perón 1185 <br><br> <b>TEL:</b> 4664-4449 <br><br> <b>Whatsapp:</b> 15-3252-3061  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.545593,-58.707374]);
+ 
 
 
 	  function initMap() {
@@ -318,9 +341,55 @@
 	    zoomOnLocation(-34.545593,-58.707374);
 	  }
 
+	window.centerGlobal = {lat: -34.562872, lng: -58.451280};
+  	window.markers=[];
+ 	window.locations=[];
+ 	window.map;
 
 
-	   function agregarLocaciones(locations){
+
+
+
+
+
+	  locations.push(['<b>SAN FERNANDO </b><br><br>Constitución 198 <br><br> <b>TEL:</b> 4506-3730 <br> <br> <b>Whatsapp:</b>  15-3252-4072   <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 13hs y 14 a 18:45hs <br> Sábados de 9 a 13hs. ',-34.435619,-58.563499]);
+
+
+	 /* locations.push(['<b>SAN JUSTO </b><br><br>Dr. Ignacio Arieta 3496 <br><br> <b>TEL:</b> 4651-0046 / 4482-4968 <br><br> <b>Whatsapp:</b> 15-3252-2397  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.681403,-58.556553]);
+
+	  locations.push(['<b>AVELLANEDA </b><br><br>Av. Mitre 531 <br><br> <b>TEL:</b> 4201-5784 / 6561 <br><br> <b>Whatsapp:</b> 15-3252-5817  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.660564,-58.367637]);
+
+	  locations.push(['<b>BERAZATEGUI </b><br><br>Av.14 N°5022 <br><br> <b>TEL:</b> 4356-0717 / 0911 <br><br> <b>Whatsapp:</b> 15-3252-4896  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.760503,-58.206957]);
+
+	  locations.push(['<b>LANUS </b><br><br>9 de Julio 1140, Local 5, Galería de las Américas <br><br> <b>TEL:</b> 4240-4798/ 5448  <br><br> <b>Whatsapp:</b> 15-3252-4303  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.708119,-58.389905]);
+
+	  locations.push(['<b>LOMAS DE ZAMORA </b><br><br>Av. Meeks 101 <br><br> <b>TEL:</b> 4244-1120 / 4243-1456 <br><br> <b>Whatsapp:</b> 15-3252-4297 <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.762114,-58.398490]);
+
+
+	  locations.push(['<b>FLORENCIO VARELA </b><br><br>Monteagudo 345 <br><br> <b>TEL:</b>4287-8544 / 8567 <br><br> <b>Whatsapp:</b> 15-3252-4920 <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.804170,-58.274902]);*/
+
+	  locations.push(['<b>QUILMES </b><br><br>Rivadavia 96   <br><br> <b>TEL:</b> 4253-1001 <br><br> <b>Whatsapp:</b> 11-6551-0711  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.722831,-58.259495]);
+	  locations.push(['<b>SAN MIGUEL </b><br><br>Presidente Perón 1185 <br><br> <b>TEL:</b> 4664-4449 <br><br> <b>Whatsapp:</b> 15-3252-3061  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.545593,-58.707374]);
+
+
+
+
+
+
+
+	  function initMap() {
+ 		map = new google.maps.Map(document.getElementById('map'), {
+	      center: centerGlobal,
+	      zoom: 9
+	    });
+
+	    agregarLocaciones(locations);
+	  }
+
+
+
+
+	  function agregarLocaciones(locations){
 			var infowindow =  new google.maps.InfoWindow({});
 			var marker,count;
 
@@ -348,14 +417,13 @@
 
 		}
 
-
-		function zoomOnLocation(latitud,longitud){
+		 function zoomOnLocation(latitud,longitud){
 	      	var center = {lat: latitud, lng: longitud};
 
 
-	      	/*if($(window).width()<1024){
+	      	if($(window).width()<1024){
 				$('html,body').animate({ scrollTop:$('#map').offset().top-50  }, 'slow');
-			}*/
+			}
 
 
 
