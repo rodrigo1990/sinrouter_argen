181894441291fe3ac1553f7c169ad97979e96fb9
rodrigo <mcd77.1990@gmail.com>
2019-10-30 14:56:55 -0300

    fix trabajar con nosotros mobile

--- app/Mail/TrabajaConNosotros.php
+++ app/Mail/TrabajaConNosotros.php
@@ -31,13 +31,29 @@ class TrabajaConNosotros extends Mailable
     
  public function build()
     {
-        return $this
-                ->subject('Argenpesos - TRABAJA CON NOSOTROS - HAZ RECIBIDO UNA SOLICITUD DE TRABAJO')
-                ->attach($this->consulta->file, 
-                     [
-                    'as' => $this->consulta->file->getClientOriginalName(),
-                    'mime' => $this->consulta->file->getClientMimeType(),
-                ])
-                ->view('mails.trabajaConNosotros');
-    }
+        if(isset($this->consulta->file)){
+        
+            return $this
+                    ->subject('Argenpesos - TRABAJA CON NOSOTROS - HAZ RECIBIDO UNA SOLICITUD DE TRABAJO')
+                    ->attach($this->consulta->file, 
+                         [
+                        'as' => $this->consulta->file->getClientOriginalName(),
+                        'mime' => $this->consulta->file->getClientMimeType(),
+                    ])
+                    ->view('mails.trabajaConNosotros');
+
+        }else if(isset($this->consulta->file2)){
+
+            return $this
+                    ->subject('Argenpesos - TRABAJA CON NOSOTROS - HAZ RECIBIDO UNA SOLICITUD DE TRABAJO')
+                    ->attach($this->consulta->file2, 
+                         [
+                        'as' => $this->consulta->file2->getClientOriginalName(),
+                        'mime' => $this->consulta->file2->getClientMimeType(),
+                    ])
+                    ->view('mails.trabajaConNosotros');
+                    
+        }//if
+
+    }//function
 }
