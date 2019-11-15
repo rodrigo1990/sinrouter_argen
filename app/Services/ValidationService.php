d9010ee9d1ac0b83fc0dccc54185adc7c4e77ae6
rodrigo1990 <mcd77.1990@gmail.com>
2019-11-15 15:12:18 -0300

    Revert "fillSiisaData avances"
    This reverts commit b2d22cf019b8b47c6bb3c57c7714b3277f2f057b.

--- app/Services/ValidationService.php
+++ app/Services/ValidationService.php
@@ -11,82 +11,40 @@ class ValidationService
 {
 
 	public function validarPrestamo($request=0){
-	
-	$client = new Client([
-	    //'headers' => ['Content-Type' => 'application/json'],
+		$client = new Client([
           // Base URI is used with relative requests
-          'base_uri' => 'http://motor.siisa.com.ar/restrequestserviceV2/',
+          'base_uri' => 'http://motor.siisa.com.ar/restrequestservice/',
           // You can set any number of default request options.
           'timeout'  => 15.0,
       ]);
 
-/*	$array = [ 
+	 $vars = array(
+ 	 	$request->nombre => 'nombre',
+ 	  	$request->apellido => 'apellido',
+ 	  	(string)$request->banco => 'nroBanco',
+ 	  	$request->provincia => 'provincia',
+ 	  	$request->empleador => 'empleador',
+ 	  	$request->sueldo => 'sueldo',
+ 	  	$request->dni => 'nroDoc',
+ 	  	$request->celular => 'celular',
+ 	  	$request->localidad => 'localidad',
+ 	  	$request->sexo => 'sexo',
+ 	  	2035365531 => 'cuil',
+ 	  	$request->valor_monto => 'monto',
+ 	  	(string)$request->fillSiisaData => 'fillSiisaData'
+ 	  );
 
-     		'clientID' => 117,
-     		'pin' => 1236,
-     		'password' => 'Legion2019',
+	 $query = http_build_query($vars, null, '/');
 
-     		'params' =>[
-				'nombre' => $request->nombre ,
-				'apellido' => $request->apellido,
-				'nroBanco' => (string)$request->banco,
-				'provincia' => $request->provincia,
-				'empleador' =>$request->empleador  ,
-				'sueldo' => $request->sueldo ,
-				'nroDoc' => $request->dni,
-				'celular' =>$request->celular ,
-				'localidad' => $request->localidad,
-				'sexo' =>$request->sexo  ,
-				'cuil' =>2035365531 ,
-				'monto' => $request->valor_monto,
-				'fillSiisaData' => (string)$request->fillSiisaData
-			]
-		];
+	 $query = str_replace("=","/", $query);
 
+	 $query = str_replace("+","%20", $query);
 
-	$array = json_encode($array);
+	 //$query = str_replace("%3D%3D","==", $query);
 
+     $response = $client->get('ExecutePolicy/clientId/117/pin/1236/password/Legion2019/policyId/35/'.$query.'/');
 
-     $response = $client->request('POST','ExecutePolicy/policyId/35',[
-     	'body' =>$array,
-
-     	 
-     ]);*/
-
-
-
-     $response = $client->request('POST','ExecutePolicy/policyId/35',[
-     	'json' =>[
-
-     		'clientID' => 117,
-     		'pin' => 1236,
-     		'password' => 'Legion2019',
-
-     		'params' =>[
-				'nombre' => $request->nombre ,
-				'apellido' => $request->apellido,
-				'nroBanco' => (string)$request->banco,
-				'provincia' => $request->provincia,
-				'empleador' =>$request->empleador  ,
-				'sueldo' => $request->sueldo ,
-				'nroDoc' => $request->dni,
-				'celular' =>$request->celular ,
-				'localidad' => $request->localidad,
-				'sexo' =>$request->sexo  ,
-				'cuil' =>2035365531 ,
-				'monto' => $request->valor_monto,
-				'fillSiisaData' => (string)$request->fillSiisaData
-			]
-
-     	]
-
-     	 
-     ]);
-
-
-
-      return $response;
-
+      dd($response->getBody()->getContents());
 
 	}
    
