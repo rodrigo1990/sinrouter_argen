2cbbfa845da6303120a83dfe1f9f1f24d9b7990b
rodrigo1990 <mcd77.1990@gmail.com>
2019-11-22 17:13:37 -0300

    cambio de datos en sucursales

--- resources/views/sucursales.blade.php
+++ resources/views/sucursales.blade.php
@@ -89,31 +89,31 @@
 
 
 
-	  locations.push(['<b>CAPITAL FEDERAL<br> </b><br>Reconquista 660, CABA <br><br> <b>TEL:</b> 4510-6594 <br><br> <b>Horario:</b> Lunes a viernes de  9 a 18hs. ',-34.599867,-58.372815]);
+	  locations.push(['<b>CAPITAL FEDERAL<br> </b><br>Reconquista 660, CABA <br><br> <b>TEL:</b> 4510-6594 <br><br> <b>Horario:</b> Lunes a viernes de  9 a 19hs <br> Sábados de 9 a 13hs.  ',-34.599867,-58.372815]);
 
-	  locations.push(['<b>SAN FERNANDO </b><br><br>Constitución 198 <br><br> <b>TEL:</b> 4506-3730 <br> <br> <b>Whatsapp:</b>  15-3252-4072   <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs <br> Sábados de 9 a 13hs. ',-34.435619,-58.563499]);
+	  locations.push(['<b>SAN FERNANDO </b><br><br>Constitución 198 <br><br> <b>TEL:</b> 4506-3730 <br> <br> <b>Whatsapp:</b>  15-3252-4072   <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 19hs <br> Sábados de 9 a 13hs. ',-34.435619,-58.563499]);
 
-	  locations.push(['<b>SAN MIGUEL </b><br><br>Presidente Perón 1185 <br><br> <b>TEL:</b> 4664-4449 <br><br> <b>Whatsapp:</b> 15-3252-3061  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.545593,-58.707374]);
+	  locations.push(['<b>SAN MIGUEL </b><br><br>Presidente Perón 1185 <br><br> <b>TEL:</b> 4664-4449 <br><br> <b>Whatsapp:</b> 15-3252-3061  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 19hs. <br> Sábados de 9 a 13hs. ',-34.545593,-58.707374]);
 
-	  locations.push(['<b>SAN JUSTO </b><br><br>Dr. Ignacio Arieta 3496 <br><br> <b>TEL:</b> 4651-0046 / 4482-4968 <br><br> <b>Whatsapp:</b> 15-3252-2397  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.681403,-58.556553]);
+	  locations.push(['<b>SAN JUSTO </b><br><br>Dr. Ignacio Arieta 3496 <br><br> <b>TEL:</b> 4651-0046 / 4482-4968 <br><br> <b>Whatsapp:</b> 15-3252-2397  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 19hs. <br> Sábados de 9 a 13hs. ',-34.681403,-58.556553]);
 
-	  locations.push(['<b>AVELLANEDA </b><br><br>Av. Mitre 531 <br><br> <b>TEL:</b> 4201-5784 / 6561 <br><br> <b>Whatsapp:</b> 15-3252-5817  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.660564,-58.367637]);
+	  locations.push(['<b>AVELLANEDA </b><br><br>Av. Mitre 531 <br><br> <b>TEL:</b> 4201-5784 / 6561 <br><br> <b>Whatsapp:</b> 15-3252-5817  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 19hs. <br> Sábados de 9 a 13hs. ',-34.660564,-58.367637]);
 
-	  locations.push(['<b>BERAZATEGUI </b><br><br>Av.14 N°5022 <br><br> <b>TEL:</b> 4356-0717 / 0911 <br><br> <b>Whatsapp:</b> 15-3252-4896  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.760503,-58.206957]);
+	  locations.push(['<b>BERAZATEGUI </b><br><br>Av.14 N°5022 <br><br> <b>TEL:</b> 4356-0717 / 0911 <br><br> <b>Whatsapp:</b> 15-3252-4896  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 19hs. <br> Sábados de 9 a 13hs. ',-34.760503,-58.206957]);
 
-	  locations.push(['<b>LANUS </b><br><br>9 de Julio 1140, Local 5, Galería de las Américas <br><br> <b>TEL:</b> 4240-4798/ 5448  <br><br> <b>Whatsapp:</b> 15-3252-4303  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.708119,-58.389905]);
+	  locations.push(['<b>LANUS </b><br><br>9 de Julio 1140, Local 5, Galería de las Américas <br><br> <b>TEL:</b> 4240-4798/ 5448  <br><br> <b>Whatsapp:</b> 15-3252-4303  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 19hs. <br> Sábados de 9 a 13hs. ',-34.708119,-58.389905]);
 
-	  locations.push(['<b>LOMAS DE ZAMORA </b><br><br>Av. Meeks 101 <br><br> <b>TEL:</b> 4244-1120 / 4243-1456 <br><br> <b>Whatsapp:</b> 15-3252-4297 <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.762114,-58.398490]);
-
-
-	  locations.push(['<b>FLORENCIO VARELA </b><br><br>Monteagudo 345 <br><br> <b>TEL:</b>4287-8544 / 8567 <br><br> <b>Whatsapp:</b> 15-3252-4920 <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.804170,-58.274902]);
-
-	  locations.push(['<b>TEMPERLEY - BARRIO SAN JOSE </b><br><br>Salta 87  <br><br> <b>TEL:</b> 2144-0093 / 3982-1412 <br><br> <b>Whatsapp:</b> 15-2165-4919  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 13:00 y de 14:00 a 18:45hs <br> Sábados de 9 a 13hs. ',-34.762226,-58.360981]);
+	  locations.push(['<b>LOMAS DE ZAMORA </b><br><br>Av. Meeks 101 <br><br> <b>TEL:</b> 4244-1120 / 4243-1456 <br><br> <b>Whatsapp:</b> 15-3252-4297 <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 19hs. <br> Sábados de 9 a 13hs. ',-34.762114,-58.398490]);
 
 
-	  locations.push(['<b>QUILMES </b><br><br>Rivadavia 96   <br><br> <b>TEL:</b> 4253-1001 <br><br> <b>Whatsapp:</b> 11-6551-0711  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 18:45hs. <br> Sábados de 9 a 13hs. ',-34.722831,-58.259495]);
+	  locations.push(['<b>FLORENCIO VARELA </b><br><br>Monteagudo 345 <br><br> <b>TEL:</b>4287-8544 / 8567 <br><br> <b>Whatsapp:</b> 15-3252-4920 <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 19hs. <br> Sábados de 9 a 13hs. ',-34.804170,-58.274902]);
 
-	  locations.push(['<b>SOLANO </b><br><br>Calle 845 Nº2590    <br><br> <b>TEL:</b> 4212-6770 / 7174<br><br> <b>Whatsapp:</b> 15-3252-5078   <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 13:00hs. y 15 a 19:30hs <br> Sábados de 9 a 13hs. ',-34.782014, -58.313454]);
+	  locations.push(['<b>TEMPERLEY - BARRIO SAN JOSE </b><br><br>Salta 87  <br><br> <b>TEL:</b> 2144-0093 / 3982-1412 <br><br> <b>Whatsapp:</b> 15-2165-4919  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 13hs y de 15:00 a 19:30hs <br> Sábados de 9 a 13hs. ',-34.762226,-58.360981]);
+
+
+	  locations.push(['<b>QUILMES </b><br><br>Rivadavia 96   <br><br> <b>TEL:</b> 4253-1001 <br><br> <b>Whatsapp:</b> 11-6551-0711  <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 19hs. <br> Sábados de 9 a 13hs. ',-34.722831,-58.259495]);
+
+	  locations.push(['<b>SOLANO </b><br><br>Calle 845 Nº2590    <br><br> <b>TEL:</b> 4212-6770 / 7174<br><br> <b>Whatsapp:</b> 15-3252-5078   <br><br> <b>Horario:</b><br> Lunes a Viernes de 9 a 19hs <br> Sábados de 9 a 13hs. ',-34.782014, -58.313454]);
 
 
 
