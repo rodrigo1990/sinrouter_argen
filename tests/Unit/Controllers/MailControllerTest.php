51f94c84b28de5f3c17b6b4a9e2689fccab75570
rodrigo1990 <mcd77.1990@gmail.com>
2019-12-06 15:08:32 -0300

    FIX EL METODO ES PREAPROBADO

--- tests/Unit/Controllers/MailControllerTest.php
+++ tests/Unit/Controllers/MailControllerTest.php
@@ -14,7 +14,7 @@ class MailControllerTest extends TestCase
      *
      * @return void
      */
-    public function testProcesarSolicitudEnviarMailRechazado()
+    public function testProcesarSolicitudEnviarMailPreaprobado()
     {
         $this->withoutExceptionHandling();
         Session::start();
