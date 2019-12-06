c897066e92949a3bce05dcbf0dafaf11dc516f65
rodrigo1990 <mcd77.1990@gmail.com>
2019-12-06 15:20:25 -0300

    Revert "fix nombre de metodo"
    This reverts commit 7233526b97d387746af98aaed6a5fdb18bafa58d.

--- tests/Unit/Controllers/MailControllerTest.php
+++ tests/Unit/Controllers/MailControllerTest.php
@@ -14,7 +14,7 @@ class MailControllerTest extends TestCase
      *
      * @return void
      */
-    public function testProcesarSolicitudEnviarMailRechazado()
+    public function testProcesarSolicitudEnviarMail()
     {
         $this->withoutExceptionHandling();
         Session::start();
