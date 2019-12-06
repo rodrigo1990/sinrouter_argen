266b69691953770f986ee58871a9f3cf30b5e7cc
rodrigo1990 <mcd77.1990@gmail.com>
2019-12-06 15:20:14 -0300

    Revert "FIX EL METODO ES PREAPROBADO"
    This reverts commit 51f94c84b28de5f3c17b6b4a9e2689fccab75570.

--- tests/Unit/Controllers/MailControllerTest.php
+++ tests/Unit/Controllers/MailControllerTest.php
@@ -14,7 +14,7 @@ class MailControllerTest extends TestCase
      *
      * @return void
      */
-    public function testProcesarSolicitudEnviarMailPreaprobado()
+    public function testProcesarSolicitudEnviarMailRechazado()
     {
         $this->withoutExceptionHandling();
         Session::start();
